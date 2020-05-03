f89ebb72075a6ed9fc3797464594b7e49bb7ad21
sanchjain <54800651+sanchjain@users.noreply.github.com>
2020-05-03 18:36:54 +0530

    updated schema for members and acad

--- hashx/hashx/schema.py
+++ hashx/hashx/schema.py
@@ -6,12 +6,19 @@ import society.schema as society_schema
 import wifipass.schema as wifipass_schema
 import timetable.schema as timetable_schema
 import shop.schema as shop_schema
+import acad.schema as acad_schema
+import members.schema as members_schema
+import acad.mutation as acad_mutations
+import members.mutation as member_mutations
 
 
-class Query(society_schema.Query, shop_schema.RelayQuery, timetable_schema.RelayQuery, wifipass_schema.Query,  graphene.ObjectType):
+class Query(acad_schema.Query, members_schema.Query, society_schema.Query, shop_schema.RelayQuery, timetable_schema.RelayQuery, wifipass_schema.Query,  graphene.ObjectType):
 
     # This Class wil inherit from multiple Queries
     # as we begin to add more apps to the project
+    pass
+
+class Mutation(acad_mutations.Mutation, member_mutations.Mutation, graphene.ObjectType):
     pass
 
 
