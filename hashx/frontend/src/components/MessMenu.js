1097ed2c5cb43a898d5d6e190118ee389ab9d8af
Aniket Biswas <abiswas_be19@thapar.edu>
2020-05-10 17:34:06 +0530

    Added the new layout

--- hashx/frontend/src/components/MessMenu.js
+++ hashx/frontend/src/components/MessMenu.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Fade from "react-reveal/Fade";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import IconButton from "@material-ui/core/IconButton";
-import Popover from "@material-ui/core/Popover";
-import { Link } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Fade from 'react-reveal/Fade';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import { Link } from 'react-router-dom';
 
 const MessMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -19,23 +19,25 @@ const MessMenu = () => {
     setAnchorEl(null);
   };
   const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
+  const id = open ? 'simple-popover' : undefined;
   return (
     <Fragment>
       <Paper
         elevation={3}
         style={{
           //   width: "550px",
-          borderRadius: "20px",
-          textAlign: "center",
-          height: "500px",
+          borderRadius: '20px',
+          textAlign: 'center',
+          height: '500px',
+          backgroundColor: '#2C3055',
+          color: '#FBF9FF',
         }}
       >
         <IconButton
           color="inherit"
           style={{
-            height: "35px",
-            float: "right",
+            height: '35px',
+            float: 'right',
           }}
           onClick={handleClick}
         >
@@ -47,17 +49,17 @@ const MessMenu = () => {
           anchorEl={anchorEl}
           onClose={handleClose}
           anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
+            vertical: 'bottom',
+            horizontal: 'center',
           }}
           transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
+            vertical: 'top',
+            horizontal: 'right',
           }}
         >
-          <Paper elevation={0} style={{ width: "" }}>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <h5 style={{ margin: "auto" }}>
+          <Paper elevation={0} style={{ width: '' }}>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <h5 style={{ margin: 'auto' }}>
                 <i class="fas fa-eye"></i> View More
               </h5>
             </Link>
@@ -65,33 +67,33 @@ const MessMenu = () => {
         </Popover>
         <br />
         <br />
-        <h1 style={{ display: "inline-block" }}>
+        <h1 style={{ display: 'inline-block' }}>
           <i
             className="fas fa-utensils fa-sm"
-            style={{ display: "inline-block" }}
-          />{" "}
+            style={{ display: 'inline-block' }}
+          />{' '}
           Mess Menu
         </h1>
         <br />
         <br />
         <Fade bottom cascade>
           <div>
-            <h3 style={{ fontWeight: "bold" }}>Breakfast</h3>
-            <h4 style={{ textAlign: "left" }}>
+            <h3 style={{ fontWeight: 'bold' }}>Breakfast</h3>
+            <h4 style={{ textAlign: 'left' }}>
               &nbsp; Food, Food, Food, Food, Food, Food, Food, Food, Food
             </h4>
           </div>
           <br />
           <div>
-            <h3 style={{ fontWeight: "bold" }}>Lunch</h3>
-            <h4 style={{ textAlign: "left" }}>
+            <h3 style={{ fontWeight: 'bold' }}>Lunch</h3>
+            <h4 style={{ textAlign: 'left' }}>
               &nbsp; Food, Food, Food, Food, Food, Food, Food, Food, Food
             </h4>
           </div>
           <br />
           <div>
-            <h3 style={{ fontWeight: "bold" }}>Dinner</h3>
-            <h4 style={{ textAlign: "left" }}>
+            <h3 style={{ fontWeight: 'bold' }}>Dinner</h3>
+            <h4 style={{ textAlign: 'left' }}>
               &nbsp; Food, Food, Food, Food, Food, Food, Food, Food, Food
             </h4>
           </div>
