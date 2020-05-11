72095916748552837750a361c125bd22306f46f4
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-11 22:31:18 +0530

    users updted schema

--- hashx/users/schema.py
+++ hashx/users/schema.py
@@ -1,24 +1,41 @@
 import graphene
+import django_filters
+from .models import Student , Instructor
+from django.db import models
 from graphene_django.types import DjangoObjectType
-from .models import Student
+from graphene_django.filter import DjangoFilterConnectionField
 
-
-class StudentType(DjangoObjectType):
-
+class StudentFilter(django_filters.FilterSet):
     class Meta:
         model = Student
+        fields = '__all__'
+        filter_overrides = {
+            models.ImageField :{
+                'filter_class' : django_filters.CharFilter,
+                'extra' : lambda f:{
+                    'lookup_expr': 'icontains'
+                }
+            }
+        }
 
+class StudentNode(DjangoObjectType):
+    class Meta:
+        model = Student
+        interfaces = (graphene.relay.Node , )
+    
+class InstructorFilter(django_filters.FilterSet):
+    class Meta:
+        model = Instructor
+        fields = '__all__'
+class InstructorNode(DjangoObjectType):
+    class Meta:
+        model = Instructor
+        interfaces = (graphene.relay.Node , )
+    
+class RelayQuery(graphene.ObjectType):
+    node = graphene.relay.Node.Field()
+    all_instructor =  DjangoFilterConnectionField(InstructorNode , filterset_class=InstructorFilter)
+    instructor = graphene.relay.Node.Field(InstructorNode)
+    all_student =  DjangoFilterConnectionField(StudentNode , filterset_class=StudentFilter)
+    student = graphene.relay.Node.Field(StudentNode)
 
-class Query(object):
-    # Student
-    student = graphene.Field(
-        StudentType,
-        id=graphene.UUID(),
-        username=graphene.String(),
-        first_name=graphene.String(),
-        last_name=graphene.String(),
-        email=graphene.String(),
-        password=graphene.String(),
-    )
-
-    all_student = graphene.List(StudentType)
