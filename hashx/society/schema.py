54633ad1137ed584f4e1527aa35e3a207b745783
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-11 22:31:00 +0530

    society mutation

--- hashx/society/schema.py
+++ hashx/society/schema.py
@@ -1,13 +1,24 @@
 import graphene
 import django_filters
 from .models import Society, Event
+from django.db import models
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+
 
 class SocietyFilter(django_filters.FilterSet):
     class Meta:
         model = Society
         fields = '__all__'
+        filter_overrides = {
+            models.ImageField :{
+                'filter_class' : django_filters.CharFilter,
+                'extra' : lambda f:{
+                    'lookup_expr': 'icontains'
+                }
+            }
+        }
+        
 class SocietyNode(DjangoObjectType):
     class Meta:
         model = Society
@@ -17,6 +28,14 @@ class EventFilter(django_filters.FilterSet):
     class Meta:
         model = Event
         fields = '__all__'
+        filter_overrides = {
+            models.ImageField :{
+                'filter_class' : django_filters.CharFilter,
+                'extra' : lambda f:{
+                    'lookup_expr': 'icontains'
+                }
+            }
+        }
 class EventNode(DjangoObjectType):
     class Meta:
         model = Event
