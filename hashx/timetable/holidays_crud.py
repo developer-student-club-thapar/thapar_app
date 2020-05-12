cf0bddfc6be29b763cf1e4fc57882c265f7797eb
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-12 06:36:09 +0530

    libraray  and laundry apps - roll v2 probabaly. don't make GraphQL api now

--- hashx/timetable/holidays_crud.py
+++ hashx/timetable/holidays_crud.py
@@ -3,29 +3,34 @@ from graphql_relay.node.node import from_global_id
 from .schema import HolidaysNode
 import graphene
 from graphene_django.types import DjangoObjectType
+
+
 class CreateHolidays(graphene.relay.ClientIDMutation):
     holiday = graphene.Field(HolidaysNode)
+
     class Input:
         name = graphene.String()
         date = graphene.types.datetime.Date()
-        
-    
+
     @classmethod
-    def mutate_and_get_payload(cls,root, info, **input):
+    def mutate_and_get_payload(cls, root, info, **input):
         name = input.get('name')
         date = input.get('date')
-        holiday = Holidays(name=name  , date = date)
+        holiday = Holidays(name=name, date=date)
         holiday.save()
-        return  CreateHolidays(holiday=holiday)
+        return CreateHolidays(holiday=holiday)
+
+
 class UpdateHolidays(graphene.relay.ClientIDMutation):
     holiday = graphene.Field(HolidaysNode)
+
     class Input:
         id = graphene.String()
         name = graphene.String()
         date = graphene.types.datetime.Date()
-        
+
     @classmethod
-    def mutate_and_get_payload(cls,root, info, **input):
+    def mutate_and_get_payload(cls, root, info, **input):
         id = input.get('id')
         id = from_global_id(id)
         id = id[1]
