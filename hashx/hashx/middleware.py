1be631e0c030a12a727258dc5c3779caa8fd8752
sanchjain <sanchjain101@gmail.com>
2020-05-17 21:47:40 +0530

    profanity filter

--- hashx/hashx/middleware.py
+++ hashx/hashx/middleware.py
@@ -1,3 +1,17 @@
+from django.conf import settings
+from profanity.validators import validate_is_profane
+from django.http import HttpResponse
+from profanity.extras import ProfanityFilter
+from django.utils.deprecation import MiddlewareMixin
+
+
+class Profanity(MiddlewareMixin):
+    def process_request(self, request):
+        # res = bytes(text, 'UTF-8')
+        pf = ProfanityFilter()
+        if pf.is_profane(request.body.decode('utf-8')) is True:
+            raise Exception("Swear word")
+        return None
 
 
 class CustomAuthorizationMiddleware(object):
