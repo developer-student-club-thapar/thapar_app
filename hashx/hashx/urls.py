027cdd6d28ceafd775e5ff971046eae9b5e31639
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-17 23:46:03 +0530

    signup form design

--- hashx/hashx/urls.py
+++ hashx/hashx/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('', include('frontend.urls'), name='home'),
     path('register/', CreateUser.as_view(), name='register'),
     path('register/create_profile/', CreateStudent.as_view(), name='create_profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/register.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path('profile/', user_views.profile, name='profile'),
     # path('', include('users.urls'))
