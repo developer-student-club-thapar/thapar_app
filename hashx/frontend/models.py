3f6612e7df9cdbcd08297a4cacdd3611ede0a995
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-17 18:58:56 +0530

    snippet model

--- hashx/frontend/models.py
+++ hashx/frontend/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 # Create Data Models to Store Analytics created by The user
+
+
+class Snippet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.CharField(max_length=255)
