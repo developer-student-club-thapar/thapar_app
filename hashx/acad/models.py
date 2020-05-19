4ee8ed546a53f9460ab4f19ff0e8ed8c0002e36a
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-19 05:53:43 +0530

    acad model changes

--- hashx/acad/models.py
+++ hashx/acad/models.py
@@ -5,13 +5,17 @@ from django.urls import reverse
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 import uuid
+from django.utils.text import slugify
+
 # Create your models here.
 
 
 class Drivefolder(models.Model):
     """ 
     This Models Contains Drive folder IDs with there Folder Type Year and subject : w
+    It is just for internal Processing 
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30, blank=True, null=True)
     drive_id = models.CharField(max_length=33)
@@ -22,7 +26,7 @@ class Drivefolder(models.Model):
         verbose_name_plural = "Drive ID"
 
     def __str__(self):
-        return f'{self.name} {self.drive_id} {self.year} {self.file_name}'
+        return f"{self.name} {self.drive_id} {self.year} {self.file_name}"
 
 
 class Course(models.Model):
@@ -38,25 +42,21 @@ class Course(models.Model):
     p = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
 
     mst = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
-    tut_ses = models.PositiveSmallIntegerField(
-        default=None, blank=True, null=True)
-    lab_proj = models.PositiveSmallIntegerField(
-        default=None, blank=True, null=True)
-    quiz = models.PositiveSmallIntegerField(
-        default=None, blank=True, null=True)
+    tut_ses = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
+    lab_proj = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
+    quiz = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
     est = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
 
-    created_date = models.DateTimeField(
-        default=timezone.now, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     # textbook = models.ForeignKey(Textbook, on_delete=models.CASCADE)
-    syllabus = models.TextField()  # Need this to makdown
+    syllabus = models.TextField()  # Need this to markdown
 
     class Meta:
-        verbose_name = ("Course")
-        verbose_name_plural = ("Courses")
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
 
     def __str__(self):
-        return f'{self.name} {self.code} '
+        return f"{self.name} {self.code} "
 
     def get_absolute_url(self):
         return reverse("course-detail", kwargs={"pk": self.pk})
@@ -66,26 +66,34 @@ class Branch(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     class Meta:
-        verbose_name = ("Branch")
-        verbose_name_plural = ("Branches")
+        verbose_name = "Branch"
+        verbose_name_plural = "Branches"
 
     YEAR_IN_SCHOOL_CHOICES = [
-        ('FR', 'Freshman'),
-        ('SO', 'Sophomore'),
-        ('JR', 'Junior Year'),
-        ('SR', 'Senior Year')
+        ("FR", "Freshman"),
+        ("SO", "Sophomore"),
+        ("JR", "Junior Year"),
+        ("SR", "Senior Year"),
     ]
 
     year = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
     code = models.CharField(max_length=3, default=None, blank=True, null=True)
     name = models.CharField(max_length=35, default=None, blank=True, null=True)
     course = models.ManyToManyField(Course, default=None, blank=True)
+    # To Label passed out branches and give them away
+    passed_out = models.BooleanField(default=False)
     # The Above Course Many To Many Field will be used always for checking out which subjects the student is categories to
-    created_date = models.DateTimeField(
-        default=timezone.now, editable=False)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
+    YEARS = [
+        (2021, 2021),
+        (2022, 2022),
+        (2023, 2023),
+        (2024, 2024),
+    ]
+    batch_of = models.IntegerField(choices=YEARS, null=True)
 
     def __str__(self):
-        return f'{self.year} {self.name} ({self.code}) {self.created_date}'
+        return f"{self.year} {self.name} ({self.code}) {self.created_date}"
 
     def get_absolute_url(self):
         return reverse("branch-detail", kwargs={"pk": self.pk})
@@ -97,6 +105,7 @@ class FirstYearBatch(models.Model):
     cause they follow a different pattern than most, rest 3 years ( 2 - 4 )
     When First Year Signs Up this is the Batch to be alloted
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     code = models.CharField(max_length=1)  # Batch Code Stuff ABCD...MNOP Part
     no = models.PositiveSmallIntegerField()  # 1..5
@@ -109,6 +118,7 @@ class Batch(models.Model):
     Valid Only for 2 - 4th Year 
     When 2-4 yearuser Signs Up this is the Batch to be alloted
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
     num = models.IntegerField()  # 1..N
@@ -116,55 +126,85 @@ class Batch(models.Model):
     created_date = models.DateTimeField(default=timezone.now, editable=False)
 
     class Meta:
-        verbose_name = ("Batch")
-        verbose_name_plural = ("Batchs")
+        verbose_name = "Batch"
+        verbose_name_plural = "Batchs"
 
     def __str__(self):
-        return f'{self.branch.year} {self.branch.code} {str(self.num)}'
+        return f"{self.branch.year} {self.branch.code} {str(self.num)}"
+
     def get_absolute_url(self):
         return reverse("batch-detail", kwargs={"pk": self.pk})
 
 
 class Textbook(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     auth_name = models.CharField(max_length=128)
     publisher = models.CharField(max_length=64)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now, editable=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    published = models.BooleanField(default=True)
+    date_modified = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(null=True)
 
     class Meta:
-        verbose_name = ("Textbook")
-        verbose_name_plural = ("Textbooks")
+        verbose_name = "Textbook"
+        verbose_name_plural = "Textbooks"
 
     def __str__(self):
-        return f'{self.name} {self.auth_name}'
+        return f"{self.name} {self.auth_name}"
 
     def get_absolute_url(self):
         return reverse("textbook-detail", kwargs={"pk": self.pk})
 
+    def save(self, *args, **kwargs):
+        # Slugify the name
+        self.slug = slugify(self.name)
+        self.date_modified = timezone.now()
+        super(Textbook, self).save(*args, **kwargs)
+
 
 class FileType(models.Model):
     """
-    Used to Mark weather this file is a Tutorial File, Tutorial Solution File or a Notes File. etc. depednding on
+    Used to Mark weather this file is a Tutorial File, Tutorial Solution File or a Notes File. etc. depending on
     the needs and requirements of the course 
+    Recommended pre loaded types 
+    Tutorials
+    Tutorial Solutions
+    Notes
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(null=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        # Slugfy the name for readabilty links
+        self.slug = slugify(self.name)
+        super(FileType, self).save(*args, **kwargs)
 
 
 class FileTags(models.Model):
     """
     No description needed
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64, unique=True)
 
+    slug = models.SlugField(null=True)
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        # Slugfy the name for readabilty links
+        self.slug = slugify(self.name)
+        super(FileTags, self).save(*args, **kwargs)
 
 
 class File(models.Model):
@@ -172,20 +212,24 @@ class File(models.Model):
     Not More than 10 file upload day per day per user so that traffic is handleable
     Functions work for the models need to built in.
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.ForeignKey(FileType, on_delete=models.PROTECT)
+    type = models.ForeignKey(FileType, on_delete=models.PROTECT, null=True, blank=True)
     tags = models.ManyToManyField(FileTags)
     file = models.FileField(upload_to="academic_File")
-    thumbnail_image = models.ImageField(upload_to="academic_file_thumbnails")
+    thumbnail_image = models.ImageField(
+        upload_to="academic_file_thumbnails", null=True, blank=True
+    )
     # Main Object
     date_posted = models.DateTimeField(default=timezone.now, editable=False)
+    date_modified = models.DateTimeField(default=timezone.now)
     name = models.CharField(max_length=128)
-    about = models.TextField(max_length=512, null=True)
+    about = models.TextField(max_length=512, null=True, blank=True)
     # This Allows anytype of user to post on the website checks neeeded for authrization.
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     # Connect this with any Academic Object that is required.
-    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
-    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
     # Batch Specific Academic Files are also entertained.  So That specfic stuff could be made.
     # Keep Batch Support for Later Stages
     batch = models.ForeignKey(Batch, on_delete=models.SET_NULL, null=True)
@@ -202,12 +246,19 @@ class File(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        # Slugify the name for the URL
+        self.slug = slugify(self.name)
+        self.date_modified = timezone.now()
+        super(File, self).save(*args, **kwargs)
+
 
 class AcademicCalendar(models.Model):  # Don't make mutations of this model
     """
     Refer to doc : https://drive.google.com/file/d/1NZLMKmbKw_S0W0MKPcbpGS9RUSddqBNl/view
     for poulating this Data in Database 
     """
+
     TYPES = [
         ("ODD", "ODD"),
         ("EVEN", "EVEN"),
@@ -217,3 +268,10 @@ class AcademicCalendar(models.Model):  # Don't make mutations of this model
     name = models.CharField(max_length=256)
     start_date = models.DateField()
     end_date = models.DateField()
+
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Slugify the name for the URL
+        self.slug = slugify(self.name)
+        super(AcademicCalendar, self).save(*args, **kwargs)
