0e9768041e0568411c356430b62deeac40e787cc
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-24 13:53:18 +0530

    docker test

--- hashx/hashx/settings.py
+++ hashx/hashx/settings.py
@@ -104,11 +104,14 @@ WSGI_APPLICATION = 'hashx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 GRAPHENE = {
     'MIDDLEWARE': [
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
