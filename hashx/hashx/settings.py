29bcb64bb2038be099b3a450e0fd1a1a1e128aa5
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-24 14:32:15 +0530

    test fail

--- hashx/hashx/settings.py
+++ hashx/hashx/settings.py
@@ -104,14 +104,11 @@ WSGI_APPLICATION = 'hashx.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 GRAPHENE = {
     'MIDDLEWARE': [
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
