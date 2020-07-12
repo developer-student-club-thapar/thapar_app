caf8276cfe6b2843a1e3f24a5a883c2ba6e71e85
Shreshth Arora <sarora_be18@thapar.edu>
2020-07-12 13:38:03 +0530

    changes

--- hashx/hashx/settings/base_settings.py
+++ hashx/hashx/settings/base_settings.py
@@ -134,8 +134,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media Files
-MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "login"
