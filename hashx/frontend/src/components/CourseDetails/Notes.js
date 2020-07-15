b44e1e8d66211f2e376ba684f5d01852f190cbcd
ramyak-mehra <rmehra_be19@thapar.edu>
2020-07-15 06:56:39 +0530

    couse files page

--- hashx/frontend/src/components/CourseDetails/Notes.js
+++ hashx/frontend/src/components/CourseDetails/Notes.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { useQuery } from '@apollo/react-hooks';
 import Button from '@material-ui/core/Button';
 import Bounce from 'react-reveal/Bounce';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
@@ -10,57 +11,30 @@ import Popover from '@material-ui/core/Popover';
 import { Link } from 'react-router-dom';
 import image from '../../assets/img.jpg';
 import { useHistory } from 'react-router-dom';
-import { FILE_TYPE_QUERY, FILES_QUERY } from './Queries';
+import CourseDetail from '../../pages/CourseDetail';
+import { FILE_TYPE_QUERY } from './Queries';
+import FileList from './FileList';
 
 
-const Notes = () => {
+
+const Notes = (props) => {
   let history = useHistory();
-  const { fileTypeLoading, fileTypeError, fileTypeData } = useQuery(
-    FILE_TYPE_QUERY,
-    {
-      variables: { slug: 'path' }, //add path as slug
-    },
-  );
-
-  const typeId = fileTypeData.allFiletypes.edges[0].node.id;
-  const { filesLoading, filesError, filesData } = useQuery(FILES_QUERY, {
-    variables: { course: `${courseID}`, type: `${typeId}` },
-  });
-  const tutorialsheets = [];
-  {
-    filesData.allFiles.edges.map((files) => {
-      const { thumbnailImage, name, file, id } = files.node;
-      return (
-        <Grid item xs={6} sm={6} md={6} lg={4} xl={4} key={id}>
-          <img
-            src={thumbnailImage}
-            style={{ borderRadius: '10px', cursor: 'pointer' }}
-            onClick={() => {
-              history.push(`/pdfview`); //file
-            }}
-          />
-          <div
-            style={{
-              position: 'relative',
-              bottom: '40px',
-              background: 'rgba(57, 57, 57, 0.5)',
-              width: '150px',
-              height: '40px',
-              borderRadius: '0px 0px 10px 10px',
-              cursor: 'pointer',
-              margin: 'auto',
-              textAlign: 'center',
-            }}
-            onClick={() => {
-              history.push(`/pdfview`);
-            }}
-          >
-            <div style={{ paddingTop: '10px' }}>{name}</div>
-          </div>
-        </Grid>
-      );
-    });
+  const { id: courseId, path } = props;
+  const {
+    loading: fileTypeLoading,
+    error: fileTypeError,
+    data: fileTypeData,
+  } = useQuery(FILE_TYPE_QUERY, { variables: { slug: `${path}` } });
+  if (fileTypeLoading) {
+    return <div> loading...</div>;
   }
+  if (fileTypeError) {
+    return <div>Error..</div>;
+  }
+  if (!fileTypeData || fileTypeData === undefined) {
+    return <div>No Files</div>;
+  }
+  const typeId = fileTypeData.allFiletypes.edges[0].node.id;
   return (
     <Fragment>
       <Paper
@@ -69,7 +43,6 @@ const Notes = () => {
           //   width: "550px",
           borderRadius: '20px',
           textAlign: 'center',
-
           backgroundColor: '#2C3055',
           color: '#FBF9FF',
           marginTop: '-30px',
@@ -78,7 +51,7 @@ const Notes = () => {
         <br />
         <h1 style={{ fontWeight: '400' }}>Notes/Slides</h1>
         <Grid container spacing={2}>
-          {tutorialsheets}
+          <FileList typeId={typeId} courseId={courseId} history = {history} />
         </Grid>
       </Paper>
     </Fragment>
