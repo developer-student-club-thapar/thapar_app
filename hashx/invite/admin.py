06207b11d52ba1da1b136441d58715c3e0df3822
Shreshth Arora <sarora_be18@thapar.edu>
2020-07-19 16:10:58 +0530

    new migrations

--- hashx/invite/admin.py
+++ hashx/invite/admin.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+
 from .models import Invite
+
 
 @admin.register(Invite)
 class InviteAdmin(admin.ModelAdmin):
-    raw_id_fields = ("invited_users",)
-    list_display = ('user' , 'invite_code')
+    list_display = (
+        'id',
+        'user',
+        'invite_code',
+        'can_invite',
+        'created_date',
+    )
+    list_filter = ('user', 'can_invite', 'created_date')
+    raw_id_fields = ('invited_users',)
--- hashx/invite/models.py
+++ hashx/invite/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.utils.crypto import get_random_string
 from django.contrib.auth.models import User
-from django.utils import timezone
+
 
 # Create your models here.
+
+
 class Invite(models.Model):
-    user = models.OneToOneField(User ,  related_name='Invite' , on_delete = models.PROTECT ,  )
-    invited_users = models.ManyToManyField(User , default= None , blank = True)
-    invite_code = models.CharField(max_length= 8 , null = True , unique = True)
-    can_invite = models.BooleanField(default=False)
+    user = models.OneToOneField(
+        User,  related_name='Invite', on_delete=models.PROTECT,)
+    invited_users = models.ManyToManyField(
+        User, related_name='Invitees', blank=True)
+    invite_code = models.CharField(max_length=8, null=True, unique=True)
+    can_invite = models.BooleanField(default=True)
     created_date = models.DateTimeField(
-        default=timezone.now, blank=True, null=True)
-    
+        auto_now=True, blank=True, null=True)
+
     def __str__(self):
         return f'{self.user.username}'
-    
-    
