8d75e79d460d7e1e11dc8f53b87c11b727946d51
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2020-07-23 23:59:15 +0530

    overlay-landing-page

--- hashx_frontend/src/components/Landing/Overlay/Overlay.jsx
+++ hashx_frontend/src/components/Landing/Overlay/Overlay.jsx
@@ -1,40 +1,97 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { useTransition, useSpring, useChain, config } from 'react-spring';
+import Container from '@material-ui/core/Container';
+import { useSpring, useTrail, config, animated, useChain } from 'react-spring';
+import background from '../../../assets/Overlay-menu/Sun-Tornado.svg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    [theme.breakpoints.down('sm')]: {},
-    [theme.breakpoints.up('sm')]: {},
-    [theme.breakpoints.up('md')]: {},
-    [theme.breakpoints.up('lg')]: {},
-    [theme.breakpoints.up('xl')]: {},
+    zIndex: 99,
+    position: 'absolute',
+    right: '0%',
+    top: '0%',
+    backgroundColor: 'black',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+  },
+  items: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '52px',
+
+      overflow: 'hidden',
+      justifyContent: 'center',
+      cursor: 'pointer',
+      paddingLeft: '5px',
+      paddingRight: '5px',
+      fontWeight: '900',
+      textTransform: 'uppercase',
+      padding: '20px',
+    },
+
+    [theme.breakpoints.up('sm')]: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      fontSize: '72px',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '96px',
+    },
+    [theme.breakpoints.up('xl')]: {
+      fontSize: '118px',
+    },
   },
 }));
 
-const Overlay = () => {
-  const [open, set] = useState(false);
+const Overlay = ({ isOpen }) => {
+  // const [open, set] = useState(false);
 
+  const classes = useStyles();
+  const items = [`home`, `about`, `contact`];
+
+  const springRef = useRef();
   const props = useSpring({
-    config: config.slow,
-    from: {
-      right: '0%',
-      top: '0%',
-      width: '0%',
-      height: '100%',
-      background: 'lightgreen',
-    },
+    config: config.default,
     to: {
-      right: '0%',
-      top: '0%',
-      width: '100%',
-      height: open ? '100%' : '0%',
-      background: 'lightblue',
+      height: '100vh',
+      width: isOpen ? '100vw' : '0vw',
     },
   });
-  const classes = userStyle();
 
-  return <div />;
+  const trailRef = useRef();
+  const trail = useTrail(items.length, {
+    to: { opacity: isOpen ? 1 : 0, x: isOpen ? 0 : 60 },
+  });
+
+  useChain([springRef, trailRef]);
+
+  return (
+    <animated.div
+      style={{ ...props, backgroundImage: `url(${background})` }}
+      className={classes.root}
+    >
+      <ul className={classes.items}>
+        <Container maxWidth="xs">
+          {trail.map((element, i) => {
+            const item = items[i];
+            return (
+              <animated.div
+                style={{ ...element, color: '#8167a9', margin: '5px' }}
+                key={item}
+              >
+                {item}
+              </animated.div>
+            );
+          })}
+        </Container>
+      </ul>
+    </animated.div>
+  );
 };
 
 export default Overlay;
