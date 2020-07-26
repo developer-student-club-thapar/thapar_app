7c22b662f852e3d982d948aec27cd377bcf52856
ramyak-mehra <rmehra_be19@thapar.edu>
2020-07-26 21:33:02 +0530

    jwt_cookie

--- hashx/hashx/urls.py
+++ hashx/hashx/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += [
-        path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
+        path("graphql/", jwt_cookie(GraphQLView.as_view(graphiql=True, schema=schema)))
     ]
 
     # Add urls for the debug toolbar
