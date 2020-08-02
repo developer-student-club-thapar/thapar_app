626709f6b6ec9cb1ec23db54bfe080df23bcc50f
ramyak-mehra <rmehra_be19@thapar.edu>
2020-08-02 19:19:01 +0530

    notifications admin

--- hashx/notification/admin.py
+++ hashx/notification/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'body', 'extra')
+    raw_id_fields = ('users',)
--- hashx/notification/models.py
+++ hashx/notification/models.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
 from .email import sendEmail
 from .fcm import sendNotifications
+import uuid
 
 # Create your models here.
 
 
 class Notification(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length = 250)
     body = models.TextField(max_length = 2000)
-    users = models.ManyToManyField(User , null=True , blank =True)
+    users = models.ManyToManyField(User , default=None , blank =True)
     extra = JSONField()
 
     def send_email(self):
