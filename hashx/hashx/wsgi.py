d21fbf910de6d3b773b4774b2f5ec85b34328fe6
Shreshth Arora <sarora_be18@thapar.edu>
2020-08-07 01:32:24 +0530

    shift static to server

--- hashx/hashx/wsgi.py
+++ hashx/hashx/wsgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hashx.settings.prod_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "hashx.settings.local_settings")
 
 application = get_wsgi_application()
