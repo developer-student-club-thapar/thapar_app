1426cb094239623bc31aed984e8d0d1cdfe517aa
ramyak-mehra <rmehra_be19@thapar.edu>
2020-08-09 17:00:41 +0530

    student data

--- hashx_next/components/Dashboard/Queries.js
+++ hashx_next/components/Dashboard/Queries.js
@@ -1,14 +1,34 @@
-import {gql} from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const ALL_COURSES = gql`
-query CoursesQuery {
-  allCourses {
-    edges {
-      node {
-        code
-        id
+  query CoursesQuery {
+    allCourses(first:10) {
+      edges {
+        node {
+          code
+          id
+        }
       }
     }
   }
-}
+`;
+
+export const STUDENT_DETAILS = gql`
+  query StudentDetail($user: ID!) {
+    allStudent(user: $user) {
+      edges {
+        node {
+          branch {
+            id
+            name
+            code
+          }
+          batch {
+            id
+            num
+          }
+        }
+      }
+    }
+  }
 `;
--- hashx_next/pages/dashboard.jsx
+++ hashx_next/pages/dashboard.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import {ALL_COURSES} from '../components/Dashboard/Queries'
+import React from "react";
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import { useQuery } from "@apollo/client";
+import { ALL_COURSES, STUDENT_DETAILS } from "../components/Dashboard/Queries";
 // import { useParams } from 'react-router-dom';
 import Layout, {
   Root,
@@ -14,13 +15,13 @@ import Layout, {
   getCollapseBtn,
   getContent,
   getFooter,
-} from '@mui-treasury/layout';
-import { HeaderMockUp } from '@mui-treasury/mockup/layout';
-import { TextSidebar } from '@mui-treasury/mockup/sidebars';
-import Sidebar from '../components/Dashboard/Sidebar';
+} from "@mui-treasury/layout";
+import { HeaderMockUp } from "@mui-treasury/mockup/layout";
+import { TextSidebar } from "@mui-treasury/mockup/sidebars";
+import Sidebar from "../components/Dashboard/Sidebar";
+import { initializeApollo } from "../lib/apolloClient";
 
-import HomeAlt from './dashboard/home';
-
+import HomeAlt from "./dashboard/home";
 
 const Header = getHeader(styled);
 const DrawerSidebar = getDrawerSidebar(styled);
@@ -32,63 +33,63 @@ const Content = getContent(styled);
 const scheme = Layout();
 
 scheme.configureHeader((builder) => {
-  builder.registerConfig('xs', {
-    position: 'sticky',
+  builder.registerConfig("xs", {
+    position: "sticky",
   });
 });
 
 scheme.configureEdgeSidebar((builder) => {
   builder
-    .create('primarySidebar', { anchor: 'left' })
-    .registerTemporaryConfig('xs', {
-      anchor: 'left',
-      width: '40vw',
+    .create("primarySidebar", { anchor: "left" })
+    .registerTemporaryConfig("xs", {
+      anchor: "left",
+      width: "40vw",
     })
-    .registerTemporaryConfig('sm', {
-      anchor: 'left',
-      width: '20vw',
+    .registerTemporaryConfig("sm", {
+      anchor: "left",
+      width: "20vw",
     })
-    .registerPermanentConfig('lg', {
-      width: '12vw',
+    .registerPermanentConfig("lg", {
+      width: "12vw",
       persistentBehavior: {
-        whatever_id: 'fit',
-        _others: 'none',
+        whatever_id: "fit",
+        _others: "none",
       },
     });
 });
 
 const useStyles = makeStyles(() => ({
   header: {
-    backgroundColor: '#F0F0F3',
+    backgroundColor: "#F0F0F3",
   },
   collapseBtn: {
-    color: '#fff',
+    color: "#fff",
     minWidth: 0,
     width: 40,
-    borderRadius: '50%',
-    border: 'none',
-    backgroundColor: 'rgba(0,0,0,0.24)',
-    margin: '0 auto 16px',
-    '&:hover': {
-      backgroundColor: 'rgba(0,0,0,0.38)',
+    borderRadius: "50%",
+    border: "none",
+    backgroundColor: "rgba(0,0,0,0.24)",
+    margin: "0 auto 16px",
+    "&:hover": {
+      backgroundColor: "rgba(0,0,0,0.38)",
     },
   },
   sidebar: {
-    backgroundColor: '#00293B',
-    border: 'none',
+    backgroundColor: "#00293B",
+    border: "none",
   },
   content: {
-    backgroundColor: '#F0F0F3',
+    backgroundColor: "#F0F0F3",
   },
 }));
 
 function renderPage(page) {
   console.log(page);
   switch (page) {
-    case 'home':
+    case "home":
       return <HomeAlt />;
-    case 'timetable':
-      return <HomeAlt/>;
+    case "timetable":
+      return <HomeAlt />;
     default:
       return <HomeAlt />;
   }
@@ -96,6 +97,17 @@ function renderPage(page) {
 
 const Dashboard = () => {
   const styles = useStyles();
+  const { loading, data, error } = useQuery(STUDENT_DETAILS, {
+    variables: {
+      user: "VXNlck5vZGU6MQ==",
+    },
+  });
+  if (loading) {
+    console.log(loading);
+  }
+  if (data) {
+    console.log(data);
+  }
   // const { page } = useParams();
   return (
     <Root scheme={scheme}>
@@ -116,25 +128,29 @@ const Dashboard = () => {
         </SidebarContent>
         {/* <CollapseBtn className={cx(styles.collapseBtn)} /> */}
       </DrawerSidebar>
-      <Content className={styles.content}>{renderPage('home')}</Content>
+      <Content className={styles.content}>{renderPage("home")}</Content>
     </Root>
   );
 };
 
 export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo();
 
   await apolloClient.query({
     query: ALL_COURSES,
-    })
+  });
+  await apolloClient.query({
+    query: STUDENT_DETAILS,
+    variables: {
+      user: "VXNlck5vZGU6MQ==",
+    },
+  });
 
   return {
     props: {
       initialApolloState: apolloClient.cache.extract(),
     },
-  
-  }
+  };
 }
-
 
 export default Dashboard;
