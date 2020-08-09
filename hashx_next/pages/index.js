4f54cfb3a620ce6f300d9383fa36de8c4489d2c3
ramyak-mehra <rmehra_be19@thapar.edu>
2020-08-09 13:23:57 +0530

    apollo client

--- hashx_next/pages/index.js
+++ hashx_next/pages/index.js
@@ -1,5 +1,53 @@
-function About() {
-  return <div> hy</div>;
+import { gql, useQuery, NetworkStatus } from '@apollo/client'
+import { initializeApollo } from '../lib/apolloClient'
+
+
+const GET_USERS = gql`
+{
+  allUsers{
+    edges{
+      node{
+        username
+      }
+    }
+  }
 }
+`;
+
+function About() {
+  const {loading , error , data , networkStatus} = useQuery(
+
+    GET_USERS,
+    {
+      notifyOnNetworkStatusChange: true,
+    }
+  )
+  if(error) return <div>{error}</div>
+  if(loading) return <div>{loading}</div>
+  data.allUsers.edges.map((ob)=>{
+    console.log(ob.node.username)
+  })
+  return(
+  <div>{data.allUsers.edges[0].node.username}</div>
+  )
+}
+
+
+export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: GET_USERS,
+    })
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+  
+  }
+}
+
+
 
 export default About;
