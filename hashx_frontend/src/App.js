4a130e677dc5b212bbecab6a713ce6e196c0e55d
Ramyak Mehra <55009050+ramyak-mehra@users.noreply.github.com>
2020-09-21 14:29:36 +0530

    Update App.js

--- hashx_frontend/src/App.js
+++ hashx_frontend/src/App.js
@@ -10,7 +10,7 @@ import { cache } from './graphql/Cache.js';
 import { getAccessToken } from './util/token';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql/',
+  uri: 'https://tietdev.vexio.in/graphql/',
   credentials: 'include',
 
   request: (operation) => {},
