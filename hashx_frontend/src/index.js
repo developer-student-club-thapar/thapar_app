83c377c7f472bed13e6cb63c52c49fb9ba6f7fef
Shreshth Arora <devsys@shreshtharora.com>
2021-02-11 20:59:36 +0530

    console log message change

--- hashx_frontend/src/index.js
+++ hashx_frontend/src/index.js
@@ -12,23 +12,23 @@ serviceWorker.unregister();
 
 function consolelog(){
     console.log(
-        "%c Vexio!!",
+        "%c Vexio!! ",
         "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgba(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
       );
       console.log(
-        "%c Welcome to our Console Page",
+        "%c Welcome to our Console Panel",
         "font-weight: bold; font-size: 30px; color: white; text-shadow: 3px 3px 0 rgb(217,31,38)"
       );
       console.log(
-        "%c Congratulations on Coming Here, This is just the Beginning, We are middle of making some Epic Features",
+        "%c Congratulations on Coming Here, This is just the Beginning, We are middle of making some Epic Features 💻",
         "font-weight: bold; font-size: 20px; color: cyan;"
       );
       console.log(
-        "%c If experimenting in the control bar is something that you enjoy join the team and you would be rewarded",
-        "font-weight: bold; font-size: 20px; color: yellow"
+        "%c If you hangout in developer tools and inspect element we are pretty much similar 😉",
+        "font-weight: bold; font-size: 20px; color: cyan"
       );
       console.log(
-        "%c If you wanna join us go to ==> https://join.vexio.xyz ",
+        "%c If you wanna join us go to 🚀 ==> https://join.vexio.xyz ",
         "font-weight: bold; font-size: 20px; color: yellow"
       );
       console.log(
