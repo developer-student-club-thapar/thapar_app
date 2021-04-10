d54518054787cd91b54d4812648aec3f2c775a01
Suprit Behera <supritb12@gmail.com>
2021-04-10 19:28:42 +0530

    Set skelet ForumExplore component and Forum landing page

---
+++ hashx_frontend/src/components/ForumExplore/ForumExplore.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useQuery } from '@apollo/client';
+
+const useStyles = makeStyles((theme) => ({
+  headerText: {
+    fontSize: '45px',
+    color: '#000000',
+    fontWeight: 'bolder',
+  },
+  box: {
+    padding: '30px',
+  },
+}));
+
+const ForumExplore = () => {
+  return(
+    <div />
+  )
+}
+
+export default ForumExplore;
