8bef41661c7b11be3963a71515c916a8fa7d43d7
Suprit Behera <supritb12@gmail.com>
2021-04-11 02:37:40 +0530

    Clean up and document ForumExplore

--- hashx_frontend/src/components/ForumExplore/ForumExplore.jsx
+++ hashx_frontend/src/components/ForumExplore/ForumExplore.jsx
@@ -6,14 +6,13 @@ import { useQuery } from '@apollo/client';
 import { ALL_QUESTIONS } from './Queries'
 
 import { secondaryColor, textColor } from '../../theme/theme';
-import Error from '../Error/Error';
 
+import Error from '../Error/Error';
 import LayoutWrapper from '../Layout/Layout';
 import RocketAnimation from '../RocketAnimation';
-
 import Question from '../Question'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   box: {
     padding: '30px',
   },
@@ -49,6 +48,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ForumExplore = () => {
+  // classes stores and gives you access to all the Material UI classes made using makeStyles()
   const classes = useStyles();
   const { data, loading, error } = useQuery(ALL_QUESTIONS);
   if(loading){
@@ -61,157 +61,71 @@ const ForumExplore = () => {
   if( error || data.allQuestions.edges == undefined || data.allQuestions.edges == null){
     return <Error />;
   }
+  // questions stores an array having all the questions retreived from the allQuestions query
   const questions = data.allQuestions.edges;
   
   return(
     <>
     <Box className={classes.box}>
-
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Paper elevation={3} className={classes.paperGrid}>
-          <Grid container spacing={2}>
-            <Grid item xs={9}>
-              <h3 className={classes.panelText}>
-                Forum <span style={{ color: '#747474' }}>Explore</span>
-              </h3>
-            </Grid>
-            <Grid item xs={3} className={classes.addIconGrid}>
-              <Grid container spacing={2}>
-                <Grid item xs={6} className={classes.addIconGrid}>
-                  <h5 className={classes.addText}>Add a Post&nbsp;</h5>
-                </Grid>
-                <Grid item xs={6}>
-                  <Paper elevation={3} className={classes.addGrid}>
-                    <i
-                      className="fas fa-plus fa-lg"
-                      style={{ color: '#000000' }}
-                    />
-                  </Paper>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {
-            questions.map((question) => {
-              let questionId = question.node.id;
-              let questionTitle = question.node.title
-              let questionUserName = question.node.owner.username;
-              let questionContent = question.node.content;
-              return(
-                <Question 
-                  questionId = {questionId}
-                  questionTitle = {questionTitle}
-                  questionUserName = {questionUserName}
-                  questionContent = {questionContent}
-                />
-              )
-            })
-          }
-
-          {/* <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Paper elevation={3} className={classes.discussionItem}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={2} style={{ padding: '20px' }}>
-                      <Avatar
-                        alt="Profile"
-                        src="https://picsum.photos/200/300"
-                        className={classes.profileAvatar}
-                      />
-                    </Grid>
-                    <Grid item xs={10}>
-                      <h4 className={classes.primaryGridText}>
-                        {questions[0].node.title}&nbsp;&nbsp;
-                      </h4>
-                      <h6 className={classes.profileId}>
-                        {questions[0].node.owner.username}
-                      </h6>
-                      <p>{questions[0].node.content}</p>
-                      <Grid container spacing={2}>
-                        <Grid item xs={2}>
-                          <h6 style={{ fontWeight: 'bolder' }}>
-                            <i
-                              className="far fa-heart fa-lg"
-                              style={{ color: '#E10505' }}
-                            />
-                            &nbsp;10
-                          </h6>
-                        </Grid>
-                        <Grid item xs={2}>
-                          <h6 style={{ fontWeight: 'bolder' }}>
-                            <i
-                              className="far fa-edit fa-lg"
-                              style={{ color: '#7C73F0' }}
-                            />
-                            &nbsp;10
-                          </h6>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Paper>
-              </Grid>
-            </Grid>
-
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Paper elevation={3} className={classes.paperGrid}>
             <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Paper elevation={3} className={classes.discussionItem}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={2} style={{ padding: '20px' }}>
-                      <Avatar
-                        alt="Profile"
-                        src="https://picsum.photos/200/300"
-                        className={classes.profileAvatar}
-                      />
-                    </Grid>
-                    <Grid item xs={10}>
-                      <h4 className={classes.primaryGridText}>
-                        {'Hello World'}&nbsp;&nbsp;
-                      </h4>
-                      <h6 className={classes.profileId}>
-                        {'Suprit Behera'}
-                      </h6>
-                      <p>{'This is a sample testing question, do not mind me'}</p>
-                      <Grid container spacing={2}>
-                        <Grid item xs={2}>
-                          <h6 style={{ fontWeight: 'bolder' }}>
-                            <i
-                              className="far fa-heart fa-lg"
-                              style={{ color: '#E10505' }}
-                            />
-                            &nbsp;10
-                          </h6>
-                        </Grid>
-                        <Grid item xs={2}>
-                          <h6 style={{ fontWeight: 'bolder' }}>
-                            <i
-                              className="far fa-edit fa-lg"
-                              style={{ color: '#7C73F0' }}
-                            />
-                            &nbsp;10
-                          </h6>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Paper>
+              <Grid item xs={9}>
+                <h3 className={classes.panelText}>
+                  Forum <span style={{ color: '#747474' }}>Explore</span>
+                </h3>
               </Grid>
-            </Grid> */}
+              <Grid item xs={3} className={classes.addIconGrid}>
+                <Grid container spacing={2}>
+                  <Grid item xs={6} className={classes.addIconGrid}>
+                    <h5 className={classes.addText}>Add a Post&nbsp;</h5>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Paper elevation={3} className={classes.addGrid}>
+                      <i
+                        className="fas fa-plus fa-lg"
+                        style={{ color: '#000000' }}
+                      />
+                    </Paper>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            {/* For every question stored in questions, its id, title and other properties are extracted
+                and sent to the Question component as props. The Question component then returns an entire card
+                having one single question card
+             */}
+            {
+              questions.map((question) => {
+                let questionId = question.node.id;
+                let questionTitle = question.node.title
+                let questionUserName = question.node.owner.username;
+                let questionContent = question.node.content;
+                return(
+                  <Question 
+                    questionId = {questionId}
+                    questionTitle = {questionTitle}
+                    questionUserName = {questionUserName}
+                    questionContent = {questionContent}
+                  />
+                )
+              })
+            }
+
+            {/* The Question component below is a hard-coded static question for development purposes */}
 
             <Question 
               questionId = {123}
-              questionTitle = {'Dummy Question'}
+              questionTitle = {'Dummy Static Question'}
               questionUserName = {'Dummy Author'}
-              questionContent = {'Dummy Content'}
+              questionContent = {'This is a hard-coded static question. To remove it, just comment-out/remove it from ForumExplore.jsx'}
             />
-
-            
-        </Paper>
+              
+          </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-
       </Box>
     </>
 
