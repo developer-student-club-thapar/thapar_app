a97d17775f94961f6c0cf07b26b24048b7887130
Raghav Sharma <raghav_tinker@yahoo.com>
2022-01-20 19:05:47 +0530

    updated loaddata.sh and added auth_user.json
    Signed-off-by: Raghav Sharma <raghav_tinker@yahoo.com>

--- loaddata.sh
+++ loaddata.sh
@@ -10,8 +10,14 @@ cd hashx
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py loaddata acad
+python3 manage.py loaddata acad/fixtures/acad_sem.json
+python3 manage.py loaddata acad/fixtures/acad_courses.json
+python3 manage.py loaddata acad/fixtures/acad_branches.json
+python3 manage.py loaddata acad/fixtures/acad_batches.json
+python3 manage.py loaddata acad/fixtures/acad_file.json
+python3 manage.py loaddata acad/fixtures/acad_files.json
 python3 manage.py loaddata timetable
-python3 manage.py loaddata hostel
+python3 manage.py loaddata auth_user.json
 python3 manage.py loaddata users
+python3 manage.py loaddata hostel
 python3 manage.py runserver
